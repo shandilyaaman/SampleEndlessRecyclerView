9bd03cb0abfc69c71367635b068cf030f82c87ae
amanshandilya <shandilyaaman@users.noreply.github.com>
2017-03-20 11:34:18 +0530

    Update MainActivity.java
    endless listener added

--- SampleEndlessRecyclerView/app/src/main/java/com/sample/android/MainActivity.java
+++ SampleEndlessRecyclerView/app/src/main/java/com/sample/android/MainActivity.java
@@ -2,10 +2,13 @@ package com.sample.android;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
 
+import com.endlessrecyclerview.android.EndlessRecyclerOnScrollListener;
 import com.sample.android.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
@@ -14,27 +17,45 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private List<String> mStringList;
     private int mLoadedItems = 0;
+    private SampleAdapter mSampleAdapter;
+    private ActivityMainBinding mActivityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        activityMainBinding.recyclerView.setLayoutManager(linearLayoutManager);
-        activityMainBinding.recyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this, DividerItemDecoration.VERTICAL));
+        mActivityMainBinding.recyclerView.setLayoutManager(linearLayoutManager);
+        mActivityMainBinding.recyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this, DividerItemDecoration.VERTICAL));
         mStringList = new ArrayList<>();
+        mSampleAdapter = new SampleAdapter(mStringList);
+        mActivityMainBinding.recyclerView.setAdapter(mSampleAdapter);
         addDataToList();
-        SampleAdapter sampleAdapter = new SampleAdapter(mStringList);
-        activityMainBinding.recyclerView.setAdapter(sampleAdapter);
+
+        mActivityMainBinding.recyclerView.addOnScrollListener(new EndlessRecyclerOnScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore() {
+                addDataToList();
+            }
+        });
     }
 
     private void addDataToList() {
-        for (int i = 0; i <= 30; i++) {
-            mStringList.add("SampleText : " + i);
-            mLoadedItems++;
-        }
+        mActivityMainBinding.itemProgressBar.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= 30; i++) {
+                    mStringList.add("SampleText : " + mLoadedItems);
+                    mLoadedItems++;
+                }
+                mSampleAdapter.notifyDataSetChanged();
+                mActivityMainBinding.itemProgressBar.setVisibility(View.GONE);
+            }
+        }, 1500);
+
     }
 }
